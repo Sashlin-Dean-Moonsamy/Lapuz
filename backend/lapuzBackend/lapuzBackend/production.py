01d03f7bc76ce84d157afd66171351f03a4e4c92
Sashlin <info.sashlindean@gmail.com>
2023-10-26 17:50:42 +0200

    First Commit

---
+++ backend/lapuzBackend/lapuzBackend/production.py
@@ -0,0 +1,16 @@
+from .settings import *
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+# Stored In Environment and generated using django.core.management.utils.get_random_secret_key method
+SECRET_KEY = os.environ.get('PRODUCTION_SECRET_KEY')
+
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
